9444770f605ce0a45d053ea455cea3383480249c
Chukwuka <williamsarimah@gmail.com>
2024-03-14 21:07:24 -0500

    Sprint4 project4

--- scripts/index.js
+++ scripts/index.js
@@ -29,15 +29,16 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
-const profileTitle = document.querySelector("profile__title");
-const profileDescription = document.querySelector("profile__description");
-const profileTitleInput = document.querySelector("profile-title-input");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
-  "profile-description-input"
+  "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-const cardTemplate =
-  document.querySelector("#card-template").Content.firstElementchild;
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 
 //*   Functions    *//
 function closePopop() {
@@ -56,9 +57,22 @@ function handleProfileFormSubmit(event) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal_opened");
+  profileEditModal.classList.add("modal__opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopop);
 
 profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+const cardsWrap = document.querySelector(".cards__list");
+initialCards.forEach((cardData) => cardsWrap.prepend(getCardElement(cardData)));
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  return cardElement;
+}
