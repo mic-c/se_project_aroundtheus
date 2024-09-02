6b32261c554712e5508416348dbd28dc153d65c8
Chukwuka <williamsarimah@gmail.com>
2024-09-02 12:33:33 -0500

    pull request sprint9

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ import Popup from "./Popup.js";
 export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super({ popupSelector });
-    this._popupForm = this._popupElement.querySelector("form");
+    this._popupForm = this._popupElement.querySelector(".modal__form");
     this._submitButton = this._popupElement.querySelector(".modal__button");
     this._submitButtonText = this._submitButton.textContent;
     this._handleFormSubmit = handleFormSubmit;
